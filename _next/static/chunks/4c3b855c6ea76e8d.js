2d93fcebc8bbeae7029bbc1ee72a3e894776b876
danielanapoli <dnapol01@gmail.com>
2025-09-28 11:58:50 -0400

    Updates

---
+++ _next/static/chunks/4c3b855c6ea76e8d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/ca0e897b6c2cf6b6.js",
+  "static/chunks/turbopack-830b8bcfa646c53f.js"
+])
---
+++ _next/static/chunks/bc1c981f028af054.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/ca0e897b6c2cf6b6.js",
+  "static/chunks/turbopack-918261b381abbeaa.js"
+])
