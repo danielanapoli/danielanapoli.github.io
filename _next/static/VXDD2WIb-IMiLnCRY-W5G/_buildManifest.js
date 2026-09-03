1dd59ac530ad04a894366bd342f18fedd15a0b70
Daniela Napoli <dnapoli@Danielas-MacBook-Neo.local>
2026-09-03 00:19:17 -0400

    Updates

--- _next/static/VXDD2WIb-IMiLnCRY-W5G/_buildManifest.js
+++ _next/static/VXDD2WIb-IMiLnCRY-W5G/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/_error": [
-    "./static/chunks/21bb7f61b9ae9475.js"
+    "./static/chunks/0bdf0db925777720.js"
   ],
   "__rewrites": {
     "afterFiles": [],
---
+++ _next/static/chunks/0bdf0db925777720.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a17b533a2e2be823.js",
+  "static/chunks/turbopack-afa723c62d19a9ff.js"
+])
